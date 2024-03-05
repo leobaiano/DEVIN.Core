7c1ef7982d30a126de486554b3e989b7415913c6
Leo Baiano <ljunior2005@gmail.com>
2024-03-05 08:49:47 -0300

    restructure

---
+++ README.md
@@ -0,0 +1,9 @@
+# DEVIN
+
+This is the Core of DEVIN, an open source project that aims to facilitate the creation of the base structure for developing services for distributed systems.
+
+## Requirements
+
+**For NodeJS services**
+
+* NodeJS and Yarn installed globally
--- main.sh
+++ main.sh
@@ -4,13 +4,38 @@ create_basic_structure_project() {
     clear
 
     echo "Choose the service language?"
-    LANGUAGE=$(gum choose "NodeJS" "Python")
+    LANGUAGE=$(gum choose "nodejs" "python")
     clear
 
     echo "What is the name of the service?"
     PROJECT_NAME=$(gum input --placeholder "project_name")
+    clear;
+
+    echo "Please wait while we create the base structure of the project..."
 
     mkdir "$PROJECT_NAME" && cd "$PROJECT_NAME"
+    git clone git@github.com:leobaiano/DEVIN.BaseStructure.git .
+
+    if [ -d "$LANGUAGE" ]; then
+        cp -r templates/${LANGUAGE}/* .
+
+        rm -rf templates
+    else
+        echo "An error code '#cpbs' occurred, contact the project maintainers or open an issue"
+    fi
+
+    if ["$LANGUAGE" == "nodejs"]; then
+        yarn install
+    fi
+
+    clear;
+
+    echo "Base structure created successfully!"
+    sleep 1
+
+    echo "What architecture?"
+    LANGUAGE=$(gum choose "Layer Architecture" "Clean Architecture")
+    clear
 }
 
 main() {
